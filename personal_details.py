5ff13f81ef3136c19fc8b8b6ab2a40e29b18ff29
Shravana2002 <shravananaik1826@gmail.com>
2026-02-05 23:23:25 +0530

    Update personal_details.py

--- personal_details.py
+++ personal_details.py
@@ -1,1 +1,6 @@
+print("Name: Shravana Naik")
+print("College: Visvesvaraya technological University VIAT muddenahalli chickballapur")
+print("Course: MCA")
+print("Email: shravananaik1@gmail.com")
+print("Phone: 7204687142")
 
