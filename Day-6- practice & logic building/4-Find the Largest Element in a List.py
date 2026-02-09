811ba8eeb5073bef15b088e5b3341d3e6489ab5c
Shravana2002 <shravananaik1826@gmail.com>
2026-02-09 19:38:48 +0530

    Update 4-Find the Largest Element in a List.py

--- Day-6- practice & logic building/4-Find the Largest Element in a List.py
+++ Day-6- practice & logic building/4-Find the Largest Element in a List.py
@@ -1,1 +1,9 @@
+def find_largest(numbers):
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
 
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+print("Largest number is:", find_largest(nums))
