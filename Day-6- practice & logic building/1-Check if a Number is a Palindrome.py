7227a52bbee32bfae1d2890bc9a9802663c994f4
Shravana2002 <shravananaik1826@gmail.com>
2026-02-09 19:23:42 +0530

    Update 1-Check if a Number is a Palindrome.py

--- Day-6- practice & logic building/1-Check if a Number is a Palindrome.py
+++ Day-6- practice & logic building/1-Check if a Number is a Palindrome.py
@@ -1,1 +1,10 @@
+def is_palindrome(num):
+    original = str(num)
+    return original == original[::-1]
+
+n = int(input("Enter a number: "))
+if is_palindrome(n):
+    print("Palindrome number")
+else:
+    print("Not a palindrome")
 
