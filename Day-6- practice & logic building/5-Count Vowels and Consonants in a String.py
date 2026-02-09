3f145ce7f8e6c6da51f25c5fb2dd489e0bcf777e
Shravana2002 <shravananaik1826@gmail.com>
2026-02-09 19:45:04 +0530

    Update 5-Count Vowels and Consonants in a String.py

--- Day-6- practice & logic building/5-Count Vowels and Consonants in a String.py
+++ Day-6- practice & logic building/5-Count Vowels and Consonants in a String.py
@@ -1,1 +1,17 @@
+def count_vowels_consonants(text):
+    vowels = "aeiouAEIOU"
+    v_count = 0
+    c_count = 0
+    
+    for char in text:
+        if char.isalpha():
+            if char in vowels:
+                v_count += 1
+            else:
+                c_count += 1
+    return v_count, c_count
 
+string = input("Enter a string: ")
+v, c = count_vowels_consonants(string)
+print("Vowels:", v)
+print("Consonants:", c)
