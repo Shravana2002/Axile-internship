a52dba58ee504a66c49db19dcba4fc7c858edfbf
Shravana2002 <shravananaik1826@gmail.com>
2026-02-09 19:34:00 +0530

    Update 3-Check if a Number is Armstrong.py

--- Day-6- practice & logic building/3-Check if a Number is Armstrong.py
+++ Day-6- practice & logic building/3-Check if a Number is Armstrong.py
@@ -1,1 +1,11 @@
+def is_armstrong(num):
+    digits = str(num)
+    power = len(digits)
+    total = sum(int(digit) ** power for digit in digits)
+    return total == num
 
+number = int(input("Enter a number: "))
+if is_armstrong(number):
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
