643aea356d2a808e2a2deabea8e26d69b489bde0
Shravana2002 <shravananaik1826@gmail.com>
2026-02-10 18:36:15 +0530

    Update list.py

--- Week-2 :Day-1/list.py
+++ Week-2 :Day-1/list.py
@@ -1,1 +1,9 @@
 
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+print("The list of fruits is:")
+print(fruits)
+
+print("\nDisplaying items one by one:")
+for fruit in fruits:
+    print(fruit)
