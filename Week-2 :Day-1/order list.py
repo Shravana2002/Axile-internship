da67c7ebbcef8efbff58a4cd87bf46df16acd9bc
Shravana2002 <shravananaik1826@gmail.com>
2026-02-10 18:47:48 +0530

    Update order list.py

--- Week-2 :Day-1/order list.py
+++ Week-2 :Day-1/order list.py
@@ -1,1 +1,12 @@
+numbers = [45, 12, 89, 33, 7, 21]
 
+print("Original List:")
+print(numbers)
+
+ascending_list = sorted(numbers)
+print("\nList in Ascending Order:")
+print(ascending_list)
+
+descending_list = sorted(numbers, reverse=True)
+print("\nList in Descending Order:")
+print(descending_list)
