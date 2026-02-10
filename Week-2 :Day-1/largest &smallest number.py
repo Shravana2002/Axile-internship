23fe4cc7df1647d3e3a06974e5c98f27c29d9bad
Shravana2002 <shravananaik1826@gmail.com>
2026-02-10 18:42:53 +0530

    Update largest &smallest number.py

--- Week-2 :Day-1/largest &smallest number.py
+++ Week-2 :Day-1/largest &smallest number.py
@@ -1,1 +1,14 @@
+numbers = [45, 12, 89, 23, 67, 2, 90]
 
+largest = numbers[0]
+smallest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+
+print("List of numbers:", numbers)
+print("Largest number:", largest)
+print("Smallest number:", smallest)
