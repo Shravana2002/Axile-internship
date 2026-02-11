e93be06824e60486bb4df7e6a5f69c043141316e
Shravana2002 <shravananaik1826@gmail.com>
2026-02-11 19:59:11 +0530

    Update set operation.py

--- Week:2 Day-2/set operation.py
+++ Week:2 Day-2/set operation.py
@@ -1,1 +1,18 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+union_set = set1.union(set2)
+print("\nUnion:", union_set)
+
+intersection_set = set1.intersection(set2)
+print("Intersection:", intersection_set)
+
+difference_set = set1.difference(set2)
+print("Difference (Set1 - Set2):", difference_set)
+
+symmetric_diff = set1.symmetric_difference(set2)
+print("Symmetric Difference:", symmetric_diff)
 
