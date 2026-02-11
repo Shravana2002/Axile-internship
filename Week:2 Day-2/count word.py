19e1bea3cfbffe50af2d71e7c879e6ff6fd65220
Shravana2002 <shravananaik1826@gmail.com>
2026-02-11 19:54:05 +0530

    Update count word.py

--- Week:2 Day-2/count word.py
+++ Week:2 Day-2/count word.py
@@ -1,1 +1,18 @@
+sentence = input("Enter a sentence: ")
 
+words = sentence.lower().split()
+
+word_count = {}
+
+for word in words:
+    
+    word = word.strip(".,!?;:'\"()[]{}")
+
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+print("\nWord Frequency:")
+for word, count in word_count.items():
+    print(word, ":", count)
