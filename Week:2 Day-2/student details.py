64a3a03226e5c4036f871e2e0b13fd464e155687
Shravana2002 <shravananaik1826@gmail.com>
2026-02-11 19:28:30 +0530

    Update student details.py

--- Week:2 Day-2/student details.py
+++ Week:2 Day-2/student details.py
@@ -1,1 +1,26 @@
+student = {
+    "name": "Shravana Naik",
+    "roll_number": 77,
+    "class": "MCA Grade",
+    "section": "A",
+    "age": 23,
+    "gender": "Female",
+    "marks": {
+        "Math": 88,
+        "Python": 92,
+        "English": 85
+    }
+}
+
+print("Student Details:")
+print("Name:", student["name"])
+print("Roll Number:", student["roll_number"])
+print("Class:", student["class"])
+print("Section:", student["section"])
+print("Age:", student["age"])
+print("Gender:", student["gender"])
+
+print("\nMarks:")
+for subject, score in student["marks"].items():
+    print(subject, ":", score)
 
