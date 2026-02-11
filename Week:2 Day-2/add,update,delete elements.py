f13c38d11b14cd22f19e532391bf7b76928cf0d2
Shravana2002 <shravananaik1826@gmail.com>
2026-02-11 19:40:08 +0530

    Update add,update,delete elements.py

--- Week:2 Day-2/add,update,delete elements.py
+++ Week:2 Day-2/add,update,delete elements.py
@@ -1,1 +1,17 @@
+student = {
+    "name": "Shravana Naik",
+    "age": 23,
+    "grade": "MCA"
+}
+
+print("Original Dictionary:", student)
+
+student["city"] = "Belagavi"
+print("\nAfter Adding city:", student)
+
+student["age"] = 24
+print("\nAfter Updating age:", student)
+
+del student["grade"]
+print("\nAfter Deleting grade:", student)
 
