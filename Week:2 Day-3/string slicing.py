888f93c9fc28def7e54281c7d9e44bbc6e39a407
Shravana2002 <shravananaik1826@gmail.com>
2026-02-12 18:56:15 +0530

    Update string slicing.py

--- Week:2 Day-3/string slicing.py
+++ Week:2 Day-3/string slicing.py
@@ -1,1 +1,15 @@
+text = "Hello, Python World!"
 
+print("Original String:", text)
+
+print("text[0:5] =", text[0:5])
+
+print("text[7:] =", text[7:])
+
+print("text[:5] =", text[:5])
+
+print("text[-6:] =", text[-6:])
+
+print("text[::2] =", text[::2])
+
+print("text[::-1] =", text[::-1])
