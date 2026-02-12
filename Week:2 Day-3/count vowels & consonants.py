3b219bd9c8740dc1293f7f707f03d499d8999de2
Shravana2002 <shravananaik1826@gmail.com>
2026-02-12 18:44:33 +0530

    Update count vowels & consonants.py

--- Week:2 Day-3/count vowels & consonants.py
+++ Week:2 Day-3/count vowels & consonants.py
@@ -1,1 +1,16 @@
+text = input("Enter a string: ")
 
+vowels = 0
+consonants = 0
+
+vowel_set = "aeiouAEIOU"
+
+for char in text:
+    if char.isalpha(): 
+        if char in vowel_set:
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Number of vowels:", vowels)
+print("Number of consonants:", consonants)
