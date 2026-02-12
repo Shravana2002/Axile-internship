973b436a117f3a162ba9210d893c67bd7870cb1c
Shravana2002 <shravananaik1826@gmail.com>
2026-02-12 18:31:48 +0530

    Update check palindrome.py

--- Week:2 Day-3/check palindrome.py
+++ Week:2 Day-3/check palindrome.py
@@ -1,1 +1,13 @@
+def check_palindrome(text):
 
+    text = text.replace(" ", "").lower()
+    
+    reversed_text = text[::-1]
+    
+    if text == reversed_text:
+        print("The string is a Palindrome.")
+    else:
+        print("The string is NOT a Palindrome.")
+
+string = input("Enter a string: ")
+check_palindrome(string)
