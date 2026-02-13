c6453a0de54e28814bb8767a72ea50f9585ff4a8
Shravana2002 <shravananaik1826@gmail.com>
2026-02-13 16:42:05 +0530

    Update create & write file.py

--- Week:2 Day:4/create & write file.py
+++ Week:2 Day:4/create & write file.py
@@ -1,1 +1,8 @@
+file = open("sample.txt", "w")
 
+file.write("Hello, this is my first file handling program in Python.\n")
+file.write("File handling is easy and useful.\n")
+
+file.close()
+
+print("File created and data written successfully!")
