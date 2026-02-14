79c8d26f91b98a6e0b46b0ea11a70de7d0ec2a25
Shravana2002 <shravananaik1826@gmail.com>
2026-02-14 19:34:47 +0530

    Update custom exception.py

--- Week:2-Day:5/custom exception.py
+++ Week:2-Day:5/custom exception.py
@@ -1,1 +1,21 @@
+class InvalidAgeError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+def check_age(age):
+    if age < 18:
+        raise InvalidAgeError("Age must be 18 or above!")
+    else:
+        print("Access Granted. You are eligible.")
+
+try:
+    age = int(input("Enter your age: "))
+    check_age(age)
+except InvalidAgeError as e:
+    print("Custom Exception Caught:", e)
+except ValueError:
+    print("Invalid input! Please enter a valid number.")
+finally:
+    print("Program execution completed.")
 
