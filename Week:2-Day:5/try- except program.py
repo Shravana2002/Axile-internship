7bc670fc887d68cfdd37889679ba247640bb23dc
Shravana2002 <shravananaik1826@gmail.com>
2026-02-14 19:17:31 +0530

    Update try- except program.py

--- Week:2-Day:5/try- except program.py
+++ Week:2-Day:5/try- except program.py
@@ -1,1 +1,20 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    result = num1 / num2
+    
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+except ValueError:
+    print("Error: Please enter valid integer numbers.")
+
+except Exception as e:
+    print("Unexpected Error:", e)
+
+finally:
+    print("Program executed successfully.")
 
