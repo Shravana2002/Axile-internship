60e1e1c1e229c529fb44ce3a188e670607621ff1
Shravana2002 <shravananaik1826@gmail.com>
2026-02-14 19:29:03 +0530

    Update handle file errors.py

--- Week:2-Day:5/handle file errors.py
+++ Week:2-Day:5/handle file errors.py
@@ -1,1 +1,19 @@
+try:
+    file = open("student.txt", "r")
+    content = file.read()
+    print("File Content:\n")
+    print(content)
+    
+    file.close()
+except FileNotFoundError:
+    print("Error: The file does not exist.")
+
+except PermissionError:
+    print("Error: You don't have permission to access this file.")
+
+except Exception as e:
+    print("An unexpected error occurred:", e)
+
+finally:
+    print("\nProgram completed.")
 
