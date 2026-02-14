ff8aac125d8ad477d177214adf3ea9633a5246be
Shravana2002 <shravananaik1826@gmail.com>
2026-02-14 19:24:00 +0530

    Update handle division by zero.py

--- Week:2-Day:5/handle division by zero.py
+++ Week:2-Day:5/handle division by zero.py
@@ -1,1 +1,16 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    
+    result = num1 / num2
+    
+    print("Result =", result)
 
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed!")
+
+except ValueError:
+    print("Error: Please enter valid integer numbers!")
+
+finally:
+    print("Program execution completed.")
