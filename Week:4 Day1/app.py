1793f001d4d3fd2791d0df93506095ae9fe2b1ca
Shravana2002 <shravananaik1826@gmail.com>
2026-02-24 21:04:09 +0530

    Update app.py

--- Week:4 Day1/app.py
+++ Week:4 Day1/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Flask, Welcome to Axilex Services Private ltd !"
+
+if __name__ == "__main__":
+    app.run(debug=True)
