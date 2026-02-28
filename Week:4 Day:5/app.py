52f86847c431efbf2911a9c1172b37ee3cc8f62e
Shravana2002 <shravananaik1826@gmail.com>
2026-02-28 19:33:36 +0530

    Update app.py

--- Week:4 Day:5/app.py
+++ Week:4 Day:5/app.py
@@ -1,1 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
+app = Flask(__name__)
+
+# Create database and table
+def init_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT
+        )
+    """)
+    
+    # Insert default user (only if not exists)
+    cursor.execute("SELECT * FROM users WHERE username=?", ("admin",))
+    if not cursor.fetchone():
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("admin", "1234"))
+    
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route("/")
+def home():
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = cursor.fetchone()
+        
+        conn.close()
+        
+        if user:
+            return redirect(url_for("dashboard"))
+        else:
+            error = "Invalid Username or Password"
+    
+    return render_template("login.html", error=error)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
